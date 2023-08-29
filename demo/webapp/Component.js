6b1691d729b452c9292ef43acee72799f463c3c2
jyon <shuyin1994916@outlook.com>
2023-08-29 22:24:28 +0800

    demo ui5

---
+++ demo/webapp/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+],
+    function (UIComponent) {
+        return UIComponent.extend("demo.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        })
+    })
